655d0b69f3b2d557729c2994611150903ac5728d
galaxyeye <ivincent.zhang@gmail.com>
2022-10-15 00:40:25 +0800

    First commit.

---
+++ src/main/java/org/platon/template/BasicUsage.java
@@ -0,0 +1,57 @@
+package org.platon.template;
+
+import ai.platon.pulsar.context.PulsarContexts;
+import ai.platon.pulsar.persist.WebPage;
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BasicUsage {
+    public static void main(String[] args) {
+        // create a pulsar session
+        var session = PulsarContexts.createSession();
+        // the main url we are playing with
+        var url = "https://list.jd.com/list.html?cat=652,12345,12349";
+        // load a page, fetch it from the web if it has expired or if it's being fetched for the first time
+        var page = session.load(url, "-expires 1d");
+        // parse the page content into a Jsoup document
+        var document = session.parse(page, false);
+        // do something with the document
+        // ...
+
+        // or, load and parse
+        var document2 = session.loadDocument(url, "-expires 1d");
+        // do something with the document
+        // ...
+
+        // load all pages with links specified by -outLink
+        var pages = session.loadOutPages(url, "-expires 1d -itemExpires 7d -outLink a[href~=item]");
+        // load, parse and scrape fields
+        var fields = session.scrape(url, "-expires 1d", "li[data-sku]",
+                Arrays.asList(".p-name em", ".p-price"));
+        // load, parse and scrape named fields
+        var fields2 = session.scrape(url, "-i 1d", "li[data-sku]",
+                Map.of("name", ".p-name em", "price", ".p-price"));
+
+        System.out.println("== document");
+        System.out.println(document.getTitle());
+
+        document.selectFirstOptional("title").ifPresent(System.out::println);
+
+        System.out.println("== document2");
+        System.out.println(document2.getTitle());
+        document.selectFirstOptional("title").ifPresent(System.out::println);
+
+        System.out.println("== pages");
+        System.out.println(pages.stream().map(WebPage::getUrl).collect(Collectors.toList()));
+
+        var gson = new Gson();
+        System.out.println("== fields");
+        System.out.println(gson.toJson(fields));
+
+        System.out.println("== fields2");
+        System.out.println(gson.toJson(fields2));
+    }
+}
---
+++ src/main/java/org/platon/template/ContinuousCrawler.java
@@ -0,0 +1,39 @@
+package org.platon.template;
+
+import ai.platon.pulsar.common.LinkExtractors;
+import ai.platon.pulsar.common.urls.Hyperlink;
+import ai.platon.pulsar.context.PulsarContext;
+import ai.platon.pulsar.context.PulsarContexts;
+import ai.platon.pulsar.crawl.common.url.ParsableHyperlink;
+import ai.platon.pulsar.dom.FeaturedDocument;
+import ai.platon.pulsar.dom.select.QueriesKt;
+import ai.platon.pulsar.persist.WebPage;
+import org.jsoup.nodes.Document;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ContinuousCrawler {
+
+    private static void onParse(WebPage page, FeaturedDocument document) {
+        // do something wonderful with the document
+        // System.out.println(document.title() + "\t|\t" + document.baseUri());
+
+        // we can extract links in document and then scraping them
+        var urls = document.selectHyperlinks("a[href~=/dp/]");
+        PulsarContexts.create().submitAll(urls);
+    }
+
+    public static void main(String[] args) {
+        var urls = LinkExtractors.fromResource("seeds10.txt")
+                .stream()
+                .map(seed -> new ParsableHyperlink(seed, ContinuousCrawler::onParse))
+                .collect(Collectors.toList());
+        var context = PulsarContexts.create();
+        // feel free to submit millions of urls here
+        // ...
+        context.submitAll(urls);
+
+        context.await();
+    }
+}
